34dda5016271a4800155366f282e28deee61f678
NadeemElShammay <103272357+NadeemElShammay@users.noreply.github.com>
2023-04-19 10:41:51 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// Modal Download Box 
+// Create By Giri adam
+// Codepelajar.com
+function openModal(open) {
+ document.getElementsByClassName('modal-blank-screen')[0].classList.add('active')
+}
+function closeBtn(close) {
+ document.getElementsByClassName('modal-blank-screen')[0].classList.remove('active')
+}
